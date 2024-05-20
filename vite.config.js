9a87dd7f9bf4bea87a9e480cd1e932cdcf3f4964
Technofancy Learner <Vkhatri137@gmail.com>
2024-05-20 19:55:47 +0545

    gh-pages, package n config settings -2nd commit

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base : "/bhuwankhatri.com.np/",
   plugins: [react()],
-
-  // Added to make listen it in other device within same network otherwise remove server 
-  server: {
-    host: '0.0.0.0',  // This allows the server to be accessed externally
-    port: 5173,       // The port number you want to use
-  },
 })
