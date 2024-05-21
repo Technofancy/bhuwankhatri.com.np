a182cc5abbde9e7fc4cfc73e3bb32fe8a923e610
Technofancy Learner <Vkhatri137@gmail.com>
2024-05-21 10:30:47 +0545

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : "/bhuwankhatri.com.np/",
+  base : "/", // bhuwankhatri.com.np/
   plugins: [react()],
 })
