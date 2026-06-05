f65f6a29cece4eda0e6266cac9fb091fa5fd5e59
Technofancy <Technofancy@users.noreply.github.com>
2026-06-05 03:43:58 +0000

    deploy: 52b82a68566fa76329af0af8ab9392b0bf5e1eee

--- src/components/ui/Loading.jsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react';
-
-const Loading = () => (
-  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#020617', color: '#f1f5f9' }}>
-    Loading...
-  </div>
-);
-
-export default Loading;
